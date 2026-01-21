b44a25443f2df63d7cd08c33b8f781593edef354
FrontendMaybeBackend <krzysztof231201@interia.pl>
2026-01-21 23:29:55 +0100

    validation for user entity and exception handler

--- hr.Application/Services/UserService.cs
+++ hr.Application/Services/UserService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using FluentValidation;
 using hr.Application.Dto;
 using hr.Application.Interfaces;
 using hr.Domain.Entity;
@@ -10,15 +11,22 @@ public class UserService : IUserService
 {
     private readonly IUserRepository _userRepository;
     private readonly IMapper _mapper;
+    private readonly IValidator<CreateUserDto> _validator;
     
 
-    public UserService(IUserRepository userRepository, IMapper mapper)
+    public UserService(IUserRepository userRepository, IMapper mapper, IValidator<CreateUserDto> validator)
     {
         _userRepository = userRepository;
         _mapper = mapper;
+        _validator = validator;
     }
     public async Task<UserResponseDto> CreateUser(CreateUserDto createUserDto)
     {   
+        var validationResult = await _validator.ValidateAsync(createUserDto);
+        if (!validationResult.IsValid)
+        {
+            throw new ValidationException(validationResult.Errors);
+        }
         var user = _mapper.Map<User>(createUserDto);
         await _userRepository.Create(user);
         return _mapper.Map<UserResponseDto>(user);
